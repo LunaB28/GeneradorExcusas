caf43fa41900bb1478ba4f7786243bade1ea0a81
LunaB28 <lunab2815@gmail.com>
2025-03-06 18:28:03 +0000

    ThirdProject

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
-import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generarExcusa();
 };
+
+function generarExcusa() {
+  let who = ['El perro', 'Mi abuela', 'El cartero', 'Mi pájaro'];
+  let action = ['se comió', 'pisó', 'aplastó', 'rompió'];
+  let what = ['mi tarea', 'mi teléfono', 'el auto'];
+  let when = ['antes de la clase', 'cuando dormía', 'mientras hacía ejercicio', 'durante mi almuerzo', 'mientras oraba'];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  let excuse = who[whoIndex] + ' ' + action[actionIndex] + ' ' + what[whatIndex] + ' ' + when[whenIndex];
+
+  document.getElementById('excuse').innerHTML = excuse;
+}
